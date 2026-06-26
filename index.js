f0571da6c658523244539eb9d8138f2776d631ca
Sudaice <charmingsudaice@gmail.com>
2026-06-27 01:29:40 +0300

    Refactor number guessing game logic and UI

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
-let guess;
-let limit1 = Math.floor((Math.random() * 100) + 1);
-let limit2 = Math.floor((Math.random() * 100) + 1);
+let guessInput = document.getElementById("guess");
+let button = document.getElementById("btn");
+let feedback = document.getElementById("feedback");
+let limit1 = Math.floor(Math.random() * 100 + 1);
+let limit2 = Math.floor(Math.random() * 100 + 1);
 let upperLimit = Math.max(limit1, limit2);
 let lowerLimit = Math.min(limit1, limit2);
-let correctGuess = Math.floor(Math.random() * (upperLimit - lowerLimit + 1) + lowerLimit);
-let maxTrials = 10;
-let trials = 0;
-let running = true;
-window.alert(`🎮🎮🎮WELCOME TO THE NUMBER GUESSING GAME🎮🎮🎮`);
-while(running) {
-    trials++;
-    guess = window.prompt(`Enter a number between ${lowerLimit} and ${upperLimit}.`);
-    guess = Number(guess);
-    if(trials <= maxTrials){
-        if(guess > upperLimit || guess < lowerLimit) {
-            window.alert(`${guess} is out of range! You have ${maxTrials - trials}trials left`);
-        }
-        else if(guess > (correctGuess + 10)) {
-            window.alert(`${guess} is too high! You have ${maxTrials - trials}trials left`);
-        }
-        else if(guess < (correctGuess - 10)) {
-            window.alert(`${guess} is too low! You have ${maxTrials - trials}trials left`);
-        }
-        else if(guess >= (correctGuess - 5) && (guess < correctGuess - 3)) {
-            window.alert(`${guess} is low! You have ${maxTrials - trials}trials left`);
-        }
-        else if(guess <= (correctGuess + 5) && (guess > correctGuess + 3)) {
-            window.alert(`${guess} is high! You have ${maxTrials - trials}trials left`);
-        }
-        else if(guess >= (correctGuess - 3) && (guess < correctGuess)) {
-            window.alert(`${guess} is close! You have ${maxTrials - trials}trials left`);
-        }
-        else if(guess <= (correctGuess + 3) && (guess > correctGuess)) {
-            window.alert(`${guess} is close! You have ${maxTrials - trials}trials left`);
-        }
-        else if(isNaN(guess)) {
-            window.alert(`Invalid input! The guess must me a valid number. You have ${maxTrials - trials}trials left`)
-        }
-        else if(guess == correctGuess) {
-            window.alert(`You WIN🎉! ${guess} was the correct guess.`);
-            running = false;
+let instruction = document.getElementById("instruction");
+let trial = 0;
+let maxTrials = 10;  /* Math.floor(((upperLimit - lowerLimit) / upperLimit) * 10) */
+let number = Math.floor(Math.random() * (upperLimit - lowerLimit + 1) + lowerLimit);
+instruction.textContent = `Guess a number between ${lowerLimit} and ${upperLimit}`;
+    button.onclick = function() {
+        let guess = guessInput.value;
+        guess = Number(guess);
+        trial++;
+        if(trial > maxTrials) {
+            feedback.textContent = `🎮🎮🎮GAME OVER🎮🎮🎮\nYOU LOSE! You are out of trials. The correct guess was ${number}.\nRefresh page to restart the game.`;
+            button.disabled = true;
+            return;
         }
         else{
-            window.alert(`${guess} is about close! You have ${maxTrials - trials}trials left`);
+            if(isNaN(guess)) {
+                feedback.textContent = `invalid input! Your guess is not a number. you have ${maxTrials - trial}trial(s) left`;
+            }
+            else if(guess === number) {
+                feedback.textContent = `🎮🎮🎮GAME OVER🎮🎮🎮\nCongratulations! You WON🎉. ${guess} is the correct guess\nRefresh page to restart the game.`;
+                button.disabled = true;
+                return;
+            }
+            else if(guess > upperLimit || guess < lowerLimit){
+                feedback.textContent = `${guess} is out of range. You have ${maxTrials - trial} trial(s) left!`;
+            }
+            else if(guess >= (number + 10)) {
+                feedback.textContent = `${guess} is too high. You have ${maxTrials - trial} trial(s) left!`;
+            }
+            else if(guess <= (number - 10)) {
+                feedback.textContent = `${guess} is too low. You have ${maxTrials - trial} trial(s) left!`;
+            }
+            else if(guess >= (number + 5)) {
+                feedback.textContent = `${guess} is high. You have ${maxTrials - trial} trial(s) left!`;
+            }
+            else if(guess <= (number - 5)) {
+                feedback.textContent = `${guess} is low. You have ${maxTrials - trial} trial(s) left!`;
+            }
+            else if(guess > number) {
+                feedback.textContent = `${guess} is high but close. You have ${maxTrials - trial} trial(s) left!`;
+            }
+            else if(guess < number) {
+                feedback.textContent = `${guess} is low but close. You have ${maxTrials - trial} trial(s) left!`;
+            }
         }
+            
     }
-}
-window.alert(`GAME OVER! Refresh page to restart game🎮. Bye👋🏻\nCreated by Sudaice`);
